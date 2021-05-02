504de475ee2a0d737d16450b49e6fdf9cf256c66
A-McMullen <61842123+A-McMullen@users.noreply.github.com>
2021-05-02 19:41:59 -0400

    Update benchmarks.sh

--- mariadb/benchmarks.sh
+++ mariadb/benchmarks.sh
@@ -8,7 +8,7 @@ sysbench --db-driver=mysql --mysql-user=root --mysql-db=test --range_size=100 \
 while [ $runId -lt 10 ]
 do
 	numCli=10
-	while [ $numCli -lt 180 ]
+	while [ $numCli -lt 100 ]
 	do
 		sysbench --db-driver=mysql --mysql-user=root --mysql-db=test --range_size=100  --table_size=10000 --tables=2  --threads=$numCli --events=0 --time=10  --rand-type=uniform /usr/share/sysbench/oltp_read_only.lua run
 		numCli=$(($numCli + 10))
